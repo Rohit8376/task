25f79b832d867d71aac5f4bac3ed4dc1a78c1cab
rohit pandey <1080pandey@gmail.com>
2020-09-23 10:14:15 +0530

    Updates

---
+++ precache-manifest.cbd88e063396598c1d1f91a21a6b0b82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8deea7dcc0d84520d135673004eff4ab",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "e5cdcf8c1ac9880c63ee",
+    "url": "/task/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "a5e02dad3f97657f558d",
+    "url": "/task/static/css/main.fcdc5f18.chunk.css"
+  },
+  {
+    "revision": "e5cdcf8c1ac9880c63ee",
+    "url": "/task/static/js/2.db102f94.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/task/static/js/2.db102f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e02dad3f97657f558d",
+    "url": "/task/static/js/main.92fcc9a7.chunk.js"
+  },
+  {
+    "revision": "f0a933e690a513e351b8",
+    "url": "/task/static/js/runtime-main.738f866a.js"
+  }
+]);
